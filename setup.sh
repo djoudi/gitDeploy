3d2e250b715ab30ba1623ebb835dedf9d031f0a0
Florian Wartner <hi@fwndev.me>
2014-11-21 16:06:19 +0100

    Added functions

--- setup.sh
+++ setup.sh
@@ -3,16 +3,26 @@
 echo "This will set up your deployment-environment.
 Please make sure that you specify the correct path to your webroot."
 
-read -p "Setup: Proceed?:" answer
+read -p "Setup: Proceed?:" proceed answer
 
-case "$answer" in
-  Yes|yes|Y|y|"")
-  git init
-  git clone 
-  ;;
-  No|no|N|n) echo "Cancel."
-  exit 1
-  ;;
-  *) echo "Unkown Parameter"
-  ;;
-esac
+function proceed {
+  case "$answer" in
+    Yes|yes|Y|y|"")
+    git init
+    wget https://raw.githubusercontent.com/fwndev/gitDeploy/master/Scripts/post-receive .git/hooks/post-receive
+    chmod +x .git/hooks/post-receive
+    git config receive.denyCurrentBranch ignore
+    echo "Please set up your local repository.."
+    exit 1
+    ;;
+    No|no|N|n) echo "Cancel."
+    exit 1
+    ;;
+    *) echo "Unkown Parameter"
+    ;;
+  esac
+}
+
+function quit {
+  exit
+}
